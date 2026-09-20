df351c979842c1999318918eb6bbc64f9a58041f
Qi Gao <qi.gao2012@gmail.com>
2026-09-20 23:14:44 +0800

    fix(graph): version durable identity independently

--- turbo_flow/include/turbo_flow_durable_buffer.h
+++ turbo_flow/include/turbo_flow_durable_buffer.h
@@ -9,7 +9,8 @@
 extern "C" {
 #endif
 
-#define TURBO_FLOW_DURABLE_BUFFER_API_VERSION UINT32_C(2)
+#define TURBO_FLOW_DURABLE_BUFFER_API_VERSION UINT32_C(1)
+#define TURBO_FLOW_DURABLE_IDENTITY_API_VERSION UINT32_C(2)
 #define TURBO_FLOW_DURABLE_SOURCE_ID_MAX 127u
 #define TURBO_FLOW_DURABLE_PARTITION_COMPONENT_MAX 255u
 #define TURBO_FLOW_DURABLE_ADMISSION_ID_MAX 255u
@@ -30,7 +31,7 @@ typedef struct turbo_flow_durable_identity_s {
 } turbo_flow_durable_identity_t;
 
 #define TURBO_FLOW_DURABLE_IDENTITY_INIT                                                   \
-  {sizeof(turbo_flow_durable_identity_t), TURBO_FLOW_DURABLE_BUFFER_API_VERSION,           \
+  {sizeof(turbo_flow_durable_identity_t), TURBO_FLOW_DURABLE_IDENTITY_API_VERSION,           \
    {NULL, 0u}, {NULL, 0u}, {NULL, 0u}, {NULL, 0u}, {NULL, 0u}, {NULL, 0u}, 0u}
 
 TURBO_FLOW_C_API int turbo_flow_msg_set_durable_identity(
